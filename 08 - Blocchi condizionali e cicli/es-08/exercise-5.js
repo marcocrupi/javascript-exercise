169a12a48b431d345d9b1d4368ddd9fa993d212d
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-13 08:00:14 +0200

    Add files via upload

--- 08 - Blocchi condizionali e cicli/es-08/exercise-5.js
+++ 08 - Blocchi condizionali e cicli/es-08/exercise-5.js
@@ -1,10 +1,10 @@
 function calculateAverageAge(persons) {
   let total = 0;
   let count = 0;
-  persons.forEach((element) => {
-    total += element.age;
+  for (let i = 0; i < persons.length; i++) {
+    total += persons[i].age;
     count++;
-  });
+  };
 
   const averageAge = total / count;
 
@@ -27,3 +27,35 @@ const persons = [
 const averageAge = calculateAverageAge(persons);
 console.log(persons);
 console.log("Average age:", averageAge);
+
+// ALTERNATIVE SOLUTION
+
+// function calculateAverageAge(persons) {
+//   let total = 0;
+//   let count = 0;
+//   persons.forEach((element) => {
+//     total += element.age;
+//     count++;
+//   });
+
+//   const averageAge = total / count;
+
+//   return averageAge;
+// }
+
+// const persons = [
+//   { name: "Paul", age: 16 },
+//   { name: "George", age: 17 },
+//   { name: "Lucas", age: 21 },
+//   { name: "Marco", age: 32 },
+//   { name: "Peter", age: 18 },
+//   { name: "Carl", age: 13 },
+//   { name: "Simon", age: 24 },
+//   { name: "Mark", age: 15 },
+//   { name: "Sandra", age: 34 },
+//   { name: "Alice", age: 28 },
+// ];
+
+// const averageAge = calculateAverageAge(persons);
+// console.log(persons);
+// console.log("Average age:", averageAge);
