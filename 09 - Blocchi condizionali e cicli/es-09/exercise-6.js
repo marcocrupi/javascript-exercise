61ccd763cade711313b18a724e243484176899c5
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-13 08:10:55 +0200

    Add files via upload

--- 09 - Blocchi condizionali e cicli/es-09/exercise-6.js
+++ 09 - Blocchi condizionali e cicli/es-09/exercise-6.js
@@ -1,15 +1,36 @@
 function getKeys(obj) {
-  const keys = Object.keys(obj).map((key) => key);
-  return keys
+  let objKeys = [];
+  for (let key in obj) {
+    objKeys.push(key);
+  }
+  return objKeys;
 }
 
 const person = {
-  firstName: 'John',
-  lastName: 'Doe',
+  firstName: "John",
+  lastName: "Doe",
   age: 32,
-  city: 'Rome',
-  job: 'Developer',
+  city: "Rome",
+  job: "Developer",
 };
 
 const keys = getKeys(person);
 console.log(keys);
+
+// ALTERNATIVE SOLUTION
+
+// function getKeys(obj) {
+//   const keys = Object.keys(obj).map((key) => key);
+//   return keys
+// }
+
+// const person = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 32,
+//   city: 'Rome',
+//   job: 'Developer',
+// };
+
+// const keys = getKeys(person);
+// console.log(keys);
