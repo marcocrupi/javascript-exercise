0916fc4ecc1be0b42714182f68728f4f84ee3a7e
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-14 07:20:34 +0200

    Add files via upload

--- 27 - Optional chaining/es-27/exercise-1.js
+++ 27 - Optional chaining/es-27/exercise-1.js
@@ -3,5 +3,5 @@ const person = {
   lastName: 'Doe'
 };
 
-console.log(person?.address?.city); // Argh! Cannot read property 'city' of undefined
-console.log(person?.fullName?.()); // Argh! Cannot read property 'fullName()' of undefined
+console.log(person?.address?.city);
+console.log(person?.fullName?.()); 
