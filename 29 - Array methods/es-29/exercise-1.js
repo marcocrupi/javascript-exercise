d06a6d450d82cd61a0dcab5fc162b4a0ab8df51b
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-14 11:51:22 +0200

    Add files via upload

---
+++ 29 - Array methods/es-29/exercise-1.js
@@ -0,0 +1,22 @@
+function adultFilter(persons) {
+  return persons.filter(function (obj) {
+    return obj.age > 18;
+  });
+}
+
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
