7c5d81fd45009c4d4c3438fe6045230e2410530c
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-15 09:29:50 +0200

    Delete Exercises/34 - Le classi directory

--- Exercises/34 - Le classi/README.md
+++
@@ -1,2 +0,0 @@
-# Classes - Exercise 1
-Definire la classe `Person` che accetta due parametri nel costruttore `firstName` e `lastName`
--- Exercises/34 - Le classi/exercise-1.js
+++
@@ -1,9 +0,0 @@
-class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-}
-
-const developer = new Person('Mario', 'Rossi');
-console.log(`${developer.firstName} ${developer.lastName}`);
