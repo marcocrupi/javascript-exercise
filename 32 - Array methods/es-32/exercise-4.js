5429d7bdb9fc7777cdf1511972b97d8d0018159f
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-15 08:24:15 +0200

    Add files via upload

---
+++ 32 - Array methods/es-32/exercise-4.js
@@ -0,0 +1,65 @@
+function uncompletedNotes(notes) {
+  let result = [];
+
+  notes.forEach((element) => {
+    if (element.todos.some((todo) => todo.done === false)) {
+      result.push(element);
+    }
+  });
+
+  return result;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: true,
+      },
+    ],
+  },
+];
+
+const notesInProgress = uncompletedNotes(notes);
+console.log("All notes: ", notes);
+console.log("Notes In Progress: ", notesInProgress);
