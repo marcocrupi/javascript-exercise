50036afc4b569ba955a6f8465bfe5a2b3200b225
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-19 09:01:08 +0200

    Add files via upload

---
+++ 50 - JSON methods/exercise-2.js
@@ -0,0 +1,19 @@
+class Person {
+  constructor(id, firstName, lastName, age,json) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+  static fromJson(json) {
+    return JSON.parse(json);
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
