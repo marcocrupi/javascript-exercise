2b40a31697b3ac95f84c36e426562479fefd0603
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-24 11:11:24 +0200

    Add files via upload

---
+++ 61 - Async Management/exercise-5.js
@@ -0,0 +1,73 @@
+const persons = [
+  {
+    id: 1,
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
+  },
+  {
+    id: 2,
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
+  },
+  {
+    id: 3,
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: "CEO",
+  },
+  {
+    id: 2,
+    jobTitle: "Project Manager",
+  },
+  {
+    id: 3,
+    jobTitle: "Developer",
+  },
+];
+
+// core here
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = persons.find((item) => item.id === id);
+
+      if (person) {
+        return resolve(person);
+      }
+
+      return reject(`Person with id: ${id} doesn't exist`);
+    }, 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const job = jobs.find((item) => item.id === id);
+
+      if (job) {
+        return resolve(job);
+      }
+
+      return reject(`Job with id: ${id} doesn't exist`);
+    }, 500);
+  });
+}
+
+// core here
+let promiseP = fetchPersonById(3);
+
+let promiseJ = fetchJobById(3);
+
+Promise.race([promiseP, promiseJ]).then((values) => {
+  console.log(values);
+});
